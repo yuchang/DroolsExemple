56b66f3c12a2bdd6a1cfb3bb24ce83a82a16a6bd
LV Yuchang <yuchang.lv@gmail.com>
2013-03-10 19:15:17 +0100

    ok'

--- src/main/java/com/sample/ProcessTest.java
+++ src/main/java/com/sample/ProcessTest.java
@@ -18,6 +18,7 @@ public class ProcessTest {
         try {
         	
             KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+            kbuilder.add(ResourceFactory.newClassPathResource("EmployeeRule.drl"), ResourceType.DRL);
             kbuilder.add(ResourceFactory.newClassPathResource("ruleflow.rf"), ResourceType.DRF);
             
             KnowledgeBuilderErrors errors = kbuilder.getErrors();
@@ -34,8 +35,23 @@ public class ProcessTest {
             StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
             KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newFileLogger(ksession, "RF_Test");
             
+            Employee employee = new Employee();
+            employee.setEduInfo("master");
+            employee.setResume("tech");
+            employee.setAnnualExam("good");
+            employee.setAwardPunish("award");
+            
+            ksession.insert(employee);
             
             ksession.startProcess("com.sample.ruleflow");
+            ksession.fireAllRules();
+            
+            System.out.println("BasicSalary: " + employee.getBasicSalary());
+            System.out.println("DutySalary: " + employee.getDutySalary());
+            System.out.println("Bonus : " + employee.getBonus());
+            System.out.println("Rate : " + employee.getPercent());
+            System.out.printf("TotalSalary: %.0f" , employee.getTotalSalary());           
+  
             logger.close();
             
         } catch (Throwable t) {
